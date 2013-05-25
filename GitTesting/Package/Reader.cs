98598170e3c1a72119b15f38973244f39ce34546
GOPbIHbI4 <93Grey10@mail.ru>
2013-05-25 19:50:42 +0400

    Первый коммит

---
+++ GitTesting/Package/Reader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTesting
+{
+    public class Reader
+    {
+        public void Read()
+        {
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GitTesting/Package/Runner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTesting
+{
+    public class Runner
+    {
+        private Reader _reader;
+        private Writer _writer;
+
+        public Runner()
+        {
+            this._writer = new Writer();
+            this._reader = new Reader();
+        }
+        public void Run()
+        {
+            this._writer.Write();
+            this._reader.Read();
+        }
+    }
+}
